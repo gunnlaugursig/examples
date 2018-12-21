4d979c2bbb94cc8cf55eabb5e6591829e9e2b6cc
gunnl <gunnl@bender>
2018-12-21 10:28:19 +0000

    Functions to retrieve data

--- spring-boot-rest/src/main/java/com/github/gunnlaugursig/controller/KataController.java
+++ spring-boot-rest/src/main/java/com/github/gunnlaugursig/controller/KataController.java
@@ -1,17 +1,34 @@
 package com.github.gunnlaugursig.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.gunnlaugursig.entity.Kata;
+import com.github.gunnlaugursig.repository.KataRepository;
 
 @RestController
 public class KataController {
 	
-	@RequestMapping("/kata")
-	public Kata getKataByName(@RequestParam(value = "name") String name) {
-		return null;
+	@Autowired
+	KataRepository repository;
+	
+	@RequestMapping("/katas")
+	public List<Kata> findAll() {
+		return repository.findAll();
+	}
+	
+	@RequestMapping("/kata/name")
+	public Kata findByName(@RequestParam(value = "name") String name) {
+		return repository.findByName(name);
+	}
+	
+	@RequestMapping("/kata/style")
+	public List<Kata> findByStyle(@RequestParam(value = "style") String style) {
+		return repository.findByStyle(style);
 	}
 
 }
