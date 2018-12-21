65e892f48a4bef3ecad42d93f9c3af6a7ab4b4dd
gunnl <gunnl@bender>
2018-12-21 10:28:44 +0000

    Operations to retrieve data

--- spring-boot-rest/src/main/java/com/github/gunnlaugursig/repository/KataRepository.java
+++ spring-boot-rest/src/main/java/com/github/gunnlaugursig/repository/KataRepository.java
@@ -1,5 +1,7 @@
 package com.github.gunnlaugursig.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
@@ -7,4 +9,6 @@ import com.github.gunnlaugursig.entity.Kata;
 
 public interface KataRepository extends Repository<Kata, Long> {
 	Kata findByName(@Param("name") String name);
+	List<Kata> findAll();
+	List<Kata> findByStyle(@Param("style") String style);
 }
