bff2fcd00cba97c34ae60779626de548e9a10e0f
gunnl <gunnl@bender>
2019-01-03 10:09:44 +0000

    Renamed project

--- spring-cloud-config-server/src/main/java/com/github/gunnlaugursig/ConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/github/gunnlaugursig/ConfigServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudConfigServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
