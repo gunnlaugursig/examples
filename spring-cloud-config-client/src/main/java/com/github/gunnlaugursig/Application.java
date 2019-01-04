d91cc263063d5868d658404d5ea8ae3b8692026c
gunnl <gunnl@bender>
2019-01-04 14:26:06 +0000

    ...

--- spring-cloud-config-client/src/main/java/com/github/gunnlaugursig/Application.java
+++ spring-cloud-config-client/src/main/java/com/github/gunnlaugursig/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloudConfigClientApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
